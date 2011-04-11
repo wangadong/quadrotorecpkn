8228900880364e5ed83cc20a5c780e4ecdc24e4d
Tonywangadong <Tonywangadong@45307b4e-578e-80b3-ef5a-041470a088e1>
2011-04-11 17:45:00 +0000

    加上了PWM的测试程序

--- Quadrotor/system/test/PWMtest.c
+++ Quadrotor/system/test/PWMtest.c
@@ -2,14 +2,13 @@
 
 void main(void){
 WDTCTL = WDTPW + WDTHOLD; // Stop WDT
-P4DIR |= (1<<4); // P1.2 and P1.3 output
-P4SEL |= (1<<4); // P1.2 and P1.3 TA1/2 otions
+P4DIR |= BIT4; // P1.2 and P1.3 output
+P4SEL |= BIT4; // P1.2 and P1.3 TA1/2 otions
 TACCR0 = 128; // PWM Period/2
 TACCTL1 = OUTMOD_6; // TACCR1 toggle/set
 TACCR1 = 96; // TACCR1 PWM duty cycle
 
 TACTL = TASSEL_2 + MC_3; // SMCLK, up-down mode
-
-__bis_SR_register(LPM0_bits); // Enter LPM0
+while(1);
 }
 
