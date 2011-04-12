2c882bf0dde939246e92dd24bc88bb2ee5ae4a7c
Tonywangadong <Tonywangadong@45307b4e-578e-80b3-ef5a-041470a088e1>
2011-04-12 15:33:15 +0000

    PWM 4.4 4.5 75%

--- Quadrotor/system/test/PWMtest.c
+++ Quadrotor/system/test/PWMtest.c
@@ -1,14 +1,16 @@
 #include "msp430x22x4.h"
 
-void main(void){
-WDTCTL = WDTPW + WDTHOLD; // Stop WDT
-P4DIR |= BIT4; // P1.2 and P1.3 output
-P4SEL |= BIT4; // P1.2 and P1.3 TA1/2 otions
-TBCCR0 = 128; // PWM Period/2
-TBCCTL1 = OUTMOD_6; // TACCR1 toggle/set
-TBCCR1 = 96; // TACCR1 PWM duty cycle
-
-TBCTL = TBSSEL_2 + MC_3; // SMCLK, up-down mode
-while(1);
+void main(void) {
+	WDTCTL = WDTPW + WDTHOLD; // Stop WDT
+	P4DIR |= BIT4 + BIT5; // P4.1 and P4.2 output
+	P4SEL |= BIT4 + BIT5; // P4.1 and P4.2 TB1/2 otions
+	TBCCR0 = 128; // PWM Period/2
+	TBCCTL1 = OUTMOD_6; // TBCCR1 toggle/set
+	TBCCR1 = 32; // TBCCR1 PWM Duty Cycle
+	TBCCTL2 = OUTMOD_6; // TBCCR2 toggle/set
+	TBCCR2 = 96; // TBCCR2 PWM duty cycle
+	TBCTL = TBSSEL_1 + MC_3; // ACLK, updown mode
+	while (1)
+		;
 }
 
