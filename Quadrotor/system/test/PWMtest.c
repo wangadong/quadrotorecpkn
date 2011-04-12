6fc14e56bed13cc34fc6a0aa2ea014204d9e7b59
xiebujiu <xiebujiu@45307b4e-578e-80b3-ef5a-041470a088e1>
2011-04-12 15:25:59 +0000

    PWM4.4是模拟的TB1，修改

--- Quadrotor/system/test/PWMtest.c
+++ Quadrotor/system/test/PWMtest.c
@@ -4,11 +4,11 @@ void main(void){
 WDTCTL = WDTPW + WDTHOLD; // Stop WDT
 P4DIR |= BIT4; // P1.2 and P1.3 output
 P4SEL |= BIT4; // P1.2 and P1.3 TA1/2 otions
-TACCR0 = 128; // PWM Period/2
-TACCTL1 = OUTMOD_6; // TACCR1 toggle/set
-TACCR1 = 96; // TACCR1 PWM duty cycle
+TBCCR0 = 128; // PWM Period/2
+TBCCTL1 = OUTMOD_6; // TACCR1 toggle/set
+TBCCR1 = 96; // TACCR1 PWM duty cycle
 
-TACTL = TASSEL_2 + MC_3; // SMCLK, up-down mode
+TBCTL = TBSSEL_2 + MC_3; // SMCLK, up-down mode
 while(1);
 }
 
