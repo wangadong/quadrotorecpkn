ef5d530cf663e7b7f00e729652b48086e8bca8f3
monkeywez <monkeywez@45307b4e-578e-80b3-ef5a-041470a088e1>
2011-04-18 15:33:53 +0000

    PWMtest

--- Quadrotor/system/test/PWMtest.c
+++ Quadrotor/system/test/PWMtest.c
@@ -1,3 +1,10 @@
+/*
+ * PWMtest.c
+ *
+ *  Created on: 2011-1-5
+ *      Author: Manu
+ */
+
 #include "msp430x22x4.h"
 
 void main(void) {
@@ -5,11 +12,11 @@ void main(void) {
 	P4DIR |= BIT4 + BIT5; // P4.1 and P4.2 output
 	P4SEL |= BIT4 + BIT5; // P4.1 and P4.2 TB1/2 otions
 	TBCCR0 = 128; // PWM Period/2
-	TBCCTL1 = OUTMOD_6; // TBCCR1 toggle/set
+	TBCCTL1 = OUTMOD_7; // TBCCR1 reset/set
 	TBCCR1 = 32; // TBCCR1 PWM Duty Cycle
-	TBCCTL2 = OUTMOD_6; // TBCCR2 toggle/set
+	TBCCTL2 = OUTMOD_7; // TBCCR2 toggle/set
 	TBCCR2 = 96; // TBCCR2 PWM duty cycle
-	TBCTL = TBSSEL_1 + MC_3; // ACLK, updown mode
+	TBCTL = TBSSEL_1 + MC_3+TBCLR; // ACLK, up mode
 	while (1)
 		;
 }
